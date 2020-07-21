d037ca7fea8baaefe063e36d63d459ee8fb6a729
Rajeev Shukla <mail2rajeevshukla@gmail.com>
2020-07-20 23:56:43 -0500

    Adding Spring cloud vault

---
+++ spring-cloud-vault/src/main/java/com/developervisits/SpringCloudVaultIntegrationApplication.java
@@ -0,0 +1,16 @@
+package com.developervisits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+
+@SpringBootApplication
+public class SpringCloudVaultIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudVaultIntegrationApplication.class, args);
+		HashMap hashMap = new HashMap();
+	}
+
+}
---
+++ spring-cloud-vault/src/test/java/com/developervisits/SpringCloudVaultIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developervisits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudVaultIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
