1f825e24a63a9926c8dcc507bc0bbafae2aaab1b
rajeevshukla <mail2rajeevshukla@gmail.com>
2020-07-21 23:21:49 -0500

    Adding bootstrap.yml file with vault configuration.

--- spring-cloud-vault/src/main/java/com/developervisits/SpringCloudVaultIntegrationApplication.java
+++ spring-cloud-vault/src/main/java/com/developervisits/SpringCloudVaultIntegrationApplication.java
@@ -1,16 +1,25 @@
 package com.developervisits;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sun.tools.jar.CommandLine;
 
 import java.util.HashMap;
 
 @SpringBootApplication
-public class SpringCloudVaultIntegrationApplication {
+public class SpringCloudVaultIntegrationApplication implements CommandLineRunner {
+
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudVaultIntegrationApplication.class, args);
-		HashMap hashMap = new HashMap();
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
