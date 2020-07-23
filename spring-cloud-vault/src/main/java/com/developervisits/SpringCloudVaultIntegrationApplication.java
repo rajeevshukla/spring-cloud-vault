d07b1701204825df0520d092e2cb721ab9491aed
rajeevshukla <mail2rajeevshukla@gmail.com>
2020-07-22 22:52:50 -0500

    Adding bootstrap.yml file with vault configuration.

--- spring-cloud-vault/src/main/java/com/developervisits/SpringCloudVaultIntegrationApplication.java
+++ spring-cloud-vault/src/main/java/com/developervisits/SpringCloudVaultIntegrationApplication.java
@@ -1,25 +1,21 @@
 package com.developervisits;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import sun.tools.jar.CommandLine;
-
-import java.util.HashMap;
 
 @SpringBootApplication
 public class SpringCloudVaultIntegrationApplication implements CommandLineRunner {
+    @Value("${oraclePassword}")
+    private String oraclePassword;
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudVaultIntegrationApplication.class, args);
+    }
 
-
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudVaultIntegrationApplication.class, args);
-
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Password fetched from vault:" + oraclePassword);
+    }
 }
